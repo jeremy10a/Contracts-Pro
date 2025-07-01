f8992044fa8bedb3ce767b0fd4c386414ed3d190
jeremy10a <jeremy@10alabs.com>
2025-07-01 15:36:52 -0400

    Delete modules directory

--- modules/finance.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def extract_payment_terms(text):
-    terms = re.findall(r'Net\s*(\d+)', text)
-    return {
-        "net_terms": [int(t) for t in terms]
-    }
--- modules/legal.py
+++
@@ -1,7 +0,0 @@
-def extract_legal_flags(text):
-    flags = {}
-    if "indemnify" in text.lower() and "uncapped" in text.lower():
-        flags["uncapped_indemnity"] = True
-    if "termination fee" in text.lower() and "$" in text:
-        flags["termination_fee"] = True
-    return flags
